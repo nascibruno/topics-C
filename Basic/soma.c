c2ca0e4284e65e2db96819d8090254beef3d96fe
nascibruno <37557220+nascibruno@users.noreply.github.com>
2019-03-31 02:33:25 -0300

    Updade folders

---
+++ Basic/soma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+int a;
+int b;
+
+printf("Digite o valor de a: ");
+scanf("%d",&a);
+printf("\n");
+
+
+printf("Digite o valor de b: ");
+scanf("%d",&b);
+printf("\n");
+
+printf("A soma de a + b : %d\n",a+b);
+
+system("pause");
+return 0;
+
+}
