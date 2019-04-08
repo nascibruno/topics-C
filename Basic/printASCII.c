ba0061f69df4caf34f76ec8e69d33de466666746
nascibruno <37557220+nascibruno@users.noreply.github.com>
2019-04-08 01:07:58 -0300

    Create printASCII.c

---
+++ Basic/printASCII.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+
+    for(int i=0; i<25;i++)printf("{dec - %d = [%c]}\n", i, i );
+        
+        for(int i=26; i<50;i++)printf("{dec - %d = [%c]}\n", i, i );
+       
+    for(int i=51; i<100;i++)printf("{dec - %d = [%c]}\n", i, i );
+        
+    for(int i=101; i<150;i++)printf("{dec - %d = [%c]}\n", i, i );
+        
+    for(int i=151; i<200;i++)printf("{dec - %d = [%c]}\n", i, i );
+       
+    for(int i=201; i<250;i++)printf("{dec - %d = [%c]}\n", i, i );
+         
+    for(int i=251; i<255;i++)printf("{dec - %d = [%c]}\n", i, i );
+       
+ 
+}
